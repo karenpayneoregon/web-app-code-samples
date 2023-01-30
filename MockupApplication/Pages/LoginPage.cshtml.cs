529cca6fd0a39ab6dad25f5fc7e3b284e27844d9
Karen Payne <paynekaren@comcast.net>
2023-01-30 12:41:37 -0800

    new projects

--- MockupApplication/Pages/LoginPage.cshtml.cs
+++ MockupApplication/Pages/LoginPage.cshtml.cs
@@ -7,72 +7,71 @@ using MockupApplication.Models;
 using Serilog;
 using Serilog.Core;
 
-namespace MockupApplication.Pages
+namespace MockupApplication.Pages;
+
+public class LoginModel : PageModel
 {
-    public class LoginModel : PageModel
+    private readonly Context _context;
+
+
+    public LoginModel(Context context)
     {
-        private readonly Context _context;
-
-
-        public LoginModel(Context context)
-        {
-            _context = context;
-            CancellationTokenSource cancellationTokenSource = new(TimeSpan.FromSeconds(1));
+        _context = context;
+        CancellationTokenSource cancellationTokenSource = new(TimeSpan.FromSeconds(1));
             
-            var success = context.CanConnectAsync(cancellationTokenSource.Token);
-            if (success == false)
-            {
-                _context.Database.EnsureDeleted();
-                _context.Database.EnsureCreated();
-            }
-
-
+        var success = context.CanConnectAsync(cancellationTokenSource.Token);
+        if (success == false)
+        {
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
         }
 
-        [BindProperty]
-        public UserLogin UserLogin { get; set; } = default!;
 
-        [BindProperty]
-        public bool InvalidLogin { get; set; }
-        public Task<IActionResult> OnGetAsync()
+    }
+
+    [BindProperty]
+    public UserLogin UserLogin { get; set; } = default!;
+
+    [BindProperty]
+    public bool InvalidLogin { get; set; }
+    public Task<IActionResult> OnGetAsync()
+    {
+        UserLogin = new UserLogin
         {
-            UserLogin = new UserLogin
-            {
-                EmailAddress = "payne@comcast.net", Pin = "12345"
-            };
+            EmailAddress = "payne@comcast.net", Pin = "12345"
+        };
 
-            Log.Information("Hello, file logger!");
+        Log.Information("Hello, file logger!");
 
-            return Task.FromResult<IActionResult>(Page());
-        }
+        return Task.FromResult<IActionResult>(Page());
+    }
 
-        public async Task<IActionResult> OnPostAsync()
-        {
+    public async Task<IActionResult> OnPostAsync()
+    {
             
 
-            if (string.IsNullOrWhiteSpace(UserLogin.EmailAddress) || string.IsNullOrWhiteSpace(UserLogin.Pin))
-            {
-                InvalidLogin = true;
-                return Page();
-            }
-
-            var result =
-                await _context.UserLogin.FirstOrDefaultAsync(x =>
-                    x.EmailAddress == UserLogin.EmailAddress && x.Pin == UserLogin.Pin);
-
-            if (result is null)
-            {
-                InvalidLogin = true;
-                return Page();
-            }
-            
-            return RedirectToPage("./UserListing");
-
-        }
-
-        private bool UserLoginExists(int id)
+        if (string.IsNullOrWhiteSpace(UserLogin.EmailAddress) || string.IsNullOrWhiteSpace(UserLogin.Pin))
         {
-          return (_context.UserLogin?.Any(e => e.Id == id)).GetValueOrDefault();
+            InvalidLogin = true;
+            return Page();
         }
+
+        var result =
+            await _context.UserLogin.FirstOrDefaultAsync(x =>
+                x.EmailAddress == UserLogin.EmailAddress && x.Pin == UserLogin.Pin);
+
+        if (result is null)
+        {
+            InvalidLogin = true;
+            return Page();
+        }
+            
+        return RedirectToPage("./UserListing");
+
+    }
+
+    private bool UserLoginExists(int id)
+    {
+        return (_context.UserLogin?.Any(e => e.Id == id)).GetValueOrDefault();
     }
 }
