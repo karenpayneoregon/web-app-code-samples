778204ff23b0db26da44b2fd466d053a2dc244ca
Karen Payne <paynekaren@comcast.net>
2023-02-08 15:08:17 -0800

    IsSectionDefined

---
+++ ConditionalLayout/Pages/AnotherPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConditionalLayout.Pages
+{
+    public class AnotherPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
