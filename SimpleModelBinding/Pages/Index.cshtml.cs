24827a67f2db9d96f1d594ae1b8898979c45a362
Karen Payne <paynekaren@comcast.net>
2023-02-14 08:36:49 -0800

    removed default logging

--- SimpleModelBinding/Pages/Index.cshtml.cs
+++ SimpleModelBinding/Pages/Index.cshtml.cs
@@ -7,10 +7,9 @@ using Serilog;
 namespace SimpleModelBinding.Pages;
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-    public IndexModel(ILogger<IndexModel> logger)
+
+    public IndexModel()
     {
-        _logger = logger;
         Introduction = new Introduction();
     }
 
