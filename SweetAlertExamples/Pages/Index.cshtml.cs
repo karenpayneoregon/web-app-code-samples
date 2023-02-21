ab76da4946465e335bb92e9bbf501bd9d85c83a0
Karen Payne <paynekaren@comcast.net>
2023-02-21 09:22:25 -0800

    new project sweet alert

---
+++ SweetAlertExamples/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Serilog;
+
+namespace SweetAlertExamples.Pages;
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        @ViewData["MessageFromOnGet"] = "Hello from OnGet";
+    }
+    [BindProperty]
+    public string? IpAddress { get; set; }
+    public void OnPostAskForIpAddress()
+    {
+        Log.Information("IP Address: {P1}", IpAddress);
+    }
+}
---
+++ SweetAlertExamples/wwwroot/js/index.js
@@ -0,0 +1,41 @@
+﻿function GetInput() {
+    (async () => {
+
+        const ipAPI = '//api.ipify.org?format=json'
+
+        const inputValue = fetch(ipAPI)
+            .then(response => response.json())
+            .then(data => data.ip)
+
+        const { value: ipAddress } = await Swal.fire({
+            title: 'Enter your IP address',
+            input: 'text',
+            inputLabel: 'Your IP address',
+            inputValue: inputValue,
+            showCancelButton: true,
+            inputValidator: (value) => {
+                if (!value) {
+                    return 'You need to write something!'
+                }
+            }
+        })
+
+        if (ipAddress) {
+            $("#_ipAddress").val(ipAddress);
+            $("#ShowResult2").text(`IP Address is ${ipAddress}`);
+        } else {
+            $("#_ipAddress").val('Empty');
+            $("#ShowResult2").text('');
+        }
+
+    })()
+}
+
+var count = 0;
+
+function AddProduct() {
+
+    count++;
+
+    $("#ShowResult1").text(count);
+}
