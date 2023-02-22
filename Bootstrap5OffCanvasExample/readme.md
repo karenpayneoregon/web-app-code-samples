0322bd6c741ae1c4b6e76db8b269c93fa7042770
Karen Payne <paynekaren@comcast.net>
2023-02-22 14:52:28 -0800

    progress-bar

--- Bootstrap5OffCanvasExample/readme.md
+++ Bootstrap5OffCanvasExample/readme.md
@@ -1,3 +1,6 @@
 ﻿# About
 
-Demonstrates Bootstrap 5 Offcanvas
+Demonstrates Bootstrap 5 
+
+- [Offcanvas](https://getbootstrap.com/docs/5.0/components/offcanvas/)
+- [Progressbar](https://getbootstrap.com/docs/5.0/components/progress/)
---
+++ Bootstrap5OffCanvasExample/wwwroot/js/ClaimsProgressbar.js
@@ -0,0 +1,65 @@
+﻿var $ClaimsProgressbar = $ClaimsProgressbar || {};
+$ClaimsProgressbar = function () {
+    var progressWord = "Progress";
+
+    var init = function (option) {
+        
+    };
+
+    //
+    // Append # to incoming value to ensure it's an
+    // identifier
+    //
+    var assertPoundSymbol = function (value) {
+
+        if (value.charAt(0) !== "#") {
+            value = "#" + value;
+        }
+
+        return value;
+    };
+
+    // Increments or deincrements the progress bar on all pages
+    var Increment = function (identifier, newValue) {
+        var currentValue = parseInt(newValue);
+
+        if (currentValue > 100) {
+            progressWord = '';
+            currentValue = 0;
+        }
+
+        $(assertPoundSymbol(identifier)).css("width", currentValue + "%").attr("aria-valuenow", currentValue).text(currentValue + "% " + progressWord);
+
+        return currentValue;
+    };
+
+    var CurrentValue = function (identifier) {
+        return parseInt($(identifier).attr("aria-valuenow"));
+    };
+    //
+    // Show progressbar by id e.g.
+    //    $ClaimsProgressbar.Show('#progressStatus')
+    //
+    var Show = function (identifier) {
+        $(assertPoundSymbol(identifier)).show();
+    };
+
+
+    //
+    // Hide progressbar by id e.g.
+    //    $ClaimsProgressbar.Hide('#progressStatus')
+    //    
+    var Hide = function (identifier) {
+        $(assertPoundSymbol(identifier)).hide();
+    };
+
+    return {
+        init: init,
+        CurrentValue: CurrentValue,
+        Increment: Increment,
+        Show: Show,
+        Hide: Hide
+
+    };
+}();
+
