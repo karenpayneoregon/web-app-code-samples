5280eb843fc2770974b28fc8391401d7b0b2848b
Karen Payne <paynekaren@comcast.net>
2023-02-23 12:13:48 -0800

    left nav bar

---
+++ ConditionalLayout/Pages/StaticLayoutPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConditionalLayout.Pages
+{
+    public class StaticLayoutPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
