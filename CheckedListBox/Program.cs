af64378c276839bb13acecc3a55d9e9fc2ddccfc
Karen Payne <paynekaren@comcast.net>
2023-02-23 08:18:53 -0800

    console bring to front

--- CheckedListBox/Program.cs
+++ CheckedListBox/Program.cs
@@ -1,4 +1,5 @@
 using CheckedListBox.Classes;
+using RazorLibrary.Classes;
 
 namespace CheckedListBox;
 
@@ -10,16 +11,13 @@ public class Program
 
         SetupLogging.Development();
 
-        // Add services to the container.
         builder.Services.AddRazorPages();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
 
@@ -31,6 +29,11 @@ public class Program
         app.UseAuthorization();
 
         app.MapRazorPages();
+
+        if (app.Environment.IsDevelopment())
+        {
+            WindowHelper.ShowConsoleWindow(app);
+        }
 
         app.Run();
     }
