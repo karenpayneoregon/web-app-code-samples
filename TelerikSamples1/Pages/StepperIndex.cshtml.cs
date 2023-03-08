05d57901d7c116d079cd0031b6701f1b6c7c785a
Karen Payne <paynekaren@comcast.net>
2023-03-08 10:23:53 -0800

    updates post js

--- TelerikSamples1/Pages/StepperIndex.cshtml.cs
+++ TelerikSamples1/Pages/StepperIndex.cshtml.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Serilog;
+
+#pragma warning disable CS8618
 
 namespace TelerikSamples1.Pages
 {
@@ -7,11 +11,31 @@ namespace TelerikSamples1.Pages
         public bool Label { get; set; }
         public bool Indicator { get; set; }
 
+        [BindProperty]
         public double Value { get; set; }
+        public Dictionary<string, bool> StepsDictionary { get; set; }
         public void OnGet()
         {            
             Label = true;
             Indicator = false;
+
+        }
+
+        public StepperIndexModel()
+        {
+            StepsDictionary = new()
+            {
+                { "Step 1", false },
+                { "Step 2", false },
+                { "Step 3", false },
+                { "Step 4", false },
+                { "Step 5", false },
+                { "Step 6", false }
+            };
+        }
+        public void OnPost()
+        {
+            Log.Information("Step: {P1}", Value +1);
         }
     }
 }
