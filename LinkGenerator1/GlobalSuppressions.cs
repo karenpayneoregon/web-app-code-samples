1829b11279836ab1a4a0f34a2074afd56fff8d4e
Karen Payne <paynekaren@comcast.net>
2023-03-13 06:49:47 -0700

    removed project

--- LinkGenerator1/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Usage", "ASP0014:Suggest using top level route registrations", Justification = "<Pending>", Scope = "member", Target = "~M:LinkGenerator1.Startup.Configure(Microsoft.AspNetCore.Builder.WebApplication,Microsoft.AspNetCore.Hosting.IWebHostEnvironment)")]
