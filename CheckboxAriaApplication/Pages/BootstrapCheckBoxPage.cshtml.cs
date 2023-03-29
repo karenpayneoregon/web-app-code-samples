e1159ac48ff7e0882217c89e954e151971718abc
Karen Payne <paynekaren@comcast.net>
2023-03-29 09:41:22 -0700

    bootstrap checkbox aria

---
+++ CheckboxAriaApplication/Pages/BootstrapCheckBoxPage.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CheckboxAriaApplication.Pages
+{
+    public class BootstrapCheckBoxPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
