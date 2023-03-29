78728fa5747fc85b5dbb306c10fc78bb28337780
Karen Payne <paynekaren@comcast.net>
2023-03-29 16:39:52 -0700

    accessibility additions

--- CheckboxAriaApplication/wwwroot/js/site.js
+++ CheckboxAriaApplication/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//
+// Dynamically change active nav item colors
+//
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelectorAll('.nav-link').forEach(link => {
+
+        link.classList.remove('text-dark');
+        link.classList.remove('bg-primary');
+
+        if (link.getAttribute('href').toLowerCase() === location.pathname.toLowerCase()) {
+            link.classList.add('text-white');
+            link.classList.add('bg-primary');
+        } else {
+            link.classList.add('text-dark');
+        }
+    });
+})
