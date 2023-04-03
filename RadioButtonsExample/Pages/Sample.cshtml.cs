d1465d5792aae6dd6b5c60c82b682288cf2858ae
Karen Payne <paynekaren@comcast.net>
2023-04-03 07:16:48 -0700

    working with screen reader

---
+++ RadioButtonsExample/Pages/Sample.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RadioButtonsExample.Pages
+{
+    public class SampleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
