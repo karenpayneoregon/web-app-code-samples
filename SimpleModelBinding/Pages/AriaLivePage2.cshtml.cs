8c1fd75547126e95153a8e976bf26990d079e8d4
Karen Payne <paynekaren@comcast.net>
2023-04-10 10:52:58 -0700

    aria live

---
+++ SimpleModelBinding/Pages/AriaLivePage2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SimpleModelBinding.Pages
+{
+    public class AriaLivePage2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
