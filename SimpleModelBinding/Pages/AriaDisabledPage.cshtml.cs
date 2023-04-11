aef9d84e047b13e97250938a8db336a33c59b61f
Karen Payne <paynekaren@comcast.net>
2023-04-11 07:31:58 -0700

    new aria live demo

---
+++ SimpleModelBinding/Pages/AriaDisabledPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SimpleModelBinding.Pages
+{
+    public class AriaDisabledPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
