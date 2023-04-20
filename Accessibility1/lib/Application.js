86fe9ff6726618748ced7f04d54efe0bf7939b97
Karen Payne <paynekaren@comcast.net>
2023-04-20 14:20:13 -0700

    :trident: application class

---
+++ Accessibility1/lib/Application.js
@@ -0,0 +1,14 @@
+﻿var $Application = $Application || {};
+$Application = function () {
+    var assignSkipper = function(currentWindow) {
+        $(currentWindow).on('keydown',
+            function (event) {
+                if (event.key === '0' && event.altKey) {
+                    $("#skipper").focus();
+                }
+            });
+    };
+    return {
+        assignSkipper: assignSkipper
+    };
+}();
--- Accessibility1/lib/workingForm1.js
+++ Accessibility1/lib/workingForm1.js
@@ -109,3 +109,4 @@ function handleDepartmentRadioClick(currentIdentifier) {
         }
     }
 }
+
