08758933856a899501e9fbcd72ebbb044ec56373
Karen Payne <paynekaren@comcast.net>
2023-04-25 08:59:59 -0700

    refinements

--- PreventDoublePostBack/Pages/Error.cshtml.cs
+++ PreventDoublePostBack/Pages/Error.cshtml.cs
@@ -7,7 +7,7 @@ namespace PreventDoublePostBack.Pages;
 [IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
