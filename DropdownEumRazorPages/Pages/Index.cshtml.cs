b9b80c3b9f1abc0b09cc8d904d597d6dbecf3253
Karen Payne <paynekaren@comcast.net>
2023-08-06 03:48:04 -0700

    updates

--- DropdownEumRazorPages/Pages/Index.cshtml.cs
+++ DropdownEumRazorPages/Pages/Index.cshtml.cs
@@ -5,12 +5,6 @@ namespace DropdownEumRazorPages.Pages;
 public class IndexModel : PageModel
 {
     public string Message { get; set; }
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
 
     [BindProperty]
     public Book Book { get; set; }
