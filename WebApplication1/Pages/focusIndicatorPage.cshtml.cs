caa681c933371dd1da343b883e4ba84cd5484baf
Karen Payne <paynekaren@comcast.net>
2023-08-28 14:52:01 -0700

    demo for focus:not(:focus-visible)

---
+++ WebApplication1/Pages/focusIndicatorPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class focusIndicatorPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
