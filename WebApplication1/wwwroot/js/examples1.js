7237a2805f1973ed14a8ad2f874b2cf61e7b6f1a
Karen Payne <paynekaren@comcast.net>
2023-10-20 10:21:32 -0700

    refactored

--- WebApplication1/wwwroot/js/examples1.js
+++ WebApplication1/wwwroot/js/examples1.js
@@ -1,32 +1,33 @@
-/**
- *
- * Provides the ability to toggle visibility of all element by classname
- *
- * @param {collection} elements - collection of a specific type
- */
-HTMLElement.prototype.toggle = function (elements) {
-    for (let index = 0; index < elements.length; index++) {
-        if (elements[index].style.display === 'none') {
-            elements[index].style.display = 'inline-block';
-        }
-        else {
-            elements[index].style.display = 'none';
-        }
-    }
-}
 
-/**
- *
- * Provides the ability to toggle disabled attribute of all element by classname
- *
- * @param {collection} elements - collection of a specific type
- */
-HTMLElement.prototype.disableToggle = function (elements) {
-    for (let index = 0; index < elements.length; index++) {
-        if (elements[index].hasAttribute('disabled')) {
-            elements[index].disabled = false;
-        } else {
-            elements[index].disabled = true;
+    /**
+     *
+     * Provides the ability to toggle visibility of all element by classname
+     *
+     * @param {collection} elements - collection of a specific type
+     */
+    HTMLElement.prototype.toggle = function (elements) {
+        for (let index = 0; index < elements.length; index++) {
+            if (elements[index].style.display === 'none') {
+                elements[index].style.display = 'inline-block';
+            }
+            else {
+                elements[index].style.display = 'none';
+            }
         }
     }
-}
+
+    /**
+     *
+     * Provides the ability to toggle disabled attribute of all element by classname
+     *
+     * @param {collection} elements - collection of a specific type
+     */
+    HTMLElement.prototype.disableToggle = function (elements) {
+        for (let index = 0; index < elements.length; index++) {
+            if (elements[index].hasAttribute('disabled')) {
+                elements[index].removeAttribute('disabled');
+            } else {
+                elements[index].setAttribute('disabled', true);
+            }
+        }
+    }
