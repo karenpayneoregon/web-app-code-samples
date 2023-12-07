864bf4c97712c4221ebd2e14a9ee7480839f3e60
Karen Payne <paynekaren@comcast.net>
2023-12-07 08:49:27 -0800

    empty text area

--- TheftReportingApp/Pages/Index.cshtml.cs
+++ TheftReportingApp/Pages/Index.cshtml.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Serilog;
+using TheftReportingApp.Classes;
+using TheftReportingApp.Models;
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
 namespace TheftReportingApp.Pages;
 public class IndexModel : PageModel
 {
+    [BindProperty]
+    public List<US_State> UsStates { get; set; }
+    public List<SelectListItem> Options { get; set; }
     public void OnGet()
     {
-        Log.Information("Greetings");
+        UsStates = StateArray.States(true);
+        Options = UsStates.Select(x => new SelectListItem()
+        {
+            Value = x.Abbreviation,
+            Text = x.Name
+        }).ToList();
     }
 }
