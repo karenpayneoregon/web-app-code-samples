3939656c96b8f1b6f8f2f17afe953b2738688a1f
Karen Payne <paynekaren@comcast.net>
2024-07-22 05:06:04 -0700

    changes

--- SecretsApp/Program.cs
+++ SecretsApp/Program.cs
@@ -7,15 +7,15 @@ internal partial class Program
     static void Main(string[] args)
     {
         var reader = new SecretAppSettingReader();
-        var secretConnection = reader.ReadSection<Connectionstrings>("Connectionstrings")
+        var connection = reader.ReadSection<Connectionstrings>(nameof(Connectionstrings))
             .DefaultConnection;
 
-        var secretMailValues = reader.ReadSection<MailSettings>("MailSettings");
+        var mail = reader.ReadSection<MailSettings>(nameof(MailSettings));
 
-        AnsiConsole.MarkupLine($"[bold yellow]Connection String:[/] {secretConnection}");
-        AnsiConsole.MarkupLine($"[bold yellow]Mail Settings From:[/] {secretMailValues.FromAddress}");
-        AnsiConsole.MarkupLine($"[bold yellow]Mail Settings Port:[/] {secretMailValues.Port}");
-        AnsiConsole.MarkupLine($"[bold yellow]Mail Settings Pickup:[/] {secretMailValues.PickupFolder}");
+        AnsiConsole.MarkupLine($"[bold yellow]Connection String:[/] {connection}");
+        AnsiConsole.MarkupLine($"[bold yellow]Mail Settings From:[/] {mail.FromAddress}");
+        AnsiConsole.MarkupLine($"[bold yellow]Mail Settings Port:[/] {mail.Port}");
+        AnsiConsole.MarkupLine($"[bold yellow]Mail Settings Pickup:[/] {mail.PickupFolder}");
 
         ExitPrompt();
     }
