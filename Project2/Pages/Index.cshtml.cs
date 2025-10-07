b1c4c00d2c00bab721c7122d1c00d77bf9e68583
Karen Payne <paynekaren@comcast.net>
2025-10-07 12:46:38 -0700

    Add new project and update libraries
    A new project named "Project2" has been added to the solution, including configuration files for build and debug settings. New files such as `settings.json`, `appsettings.json`, and `appsettings.Development.json` have been added to configure the development environment and application settings. The project includes a new `Program.cs` file for setting up an ASP.NET Core application with Razor Pages and Serilog logging. Additionally, new Razor Pages and layout files have been added to establish the basic structure of the web application.
    
    The commit also updates the Bootstrap library to version 5.3.0, introducing new components and utilities for enhanced functionality. jQuery validation scripts have been updated to version 4.0.0, with new validation methods and error handling. License headers and files have been added to ensure compliance with open-source licenses.
    
    by Karen

---
+++ Project2/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Serilog;
+
+namespace Project2.Pages;
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+        Log.Information("Greetings");
+    }
+}
